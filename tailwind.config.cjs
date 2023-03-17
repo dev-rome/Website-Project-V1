6810b5566722f9180d887309f6fff452768a136b
dev-rome <haynesjerome@gmail.com>
2023-03-17 15:44:51 -0400

    added roboto font and gradient color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "sans-serif": "Roboto",
+      },
+      backgroundImage: {
+        "hover-gradient":
+          "linear-gradient(90deg, #003A5A 0%, #005382 42.71%, #00934C 55.73%, #00522B 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
