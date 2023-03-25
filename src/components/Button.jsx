f03799d369c2e3a07a4576f186027ff7cbb47137
dev-rome <haynesjerome@gmail.com>
2023-03-24 23:07:49 -0400

    edit button style for logIn button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import className from "classnames";
 
 function Button({ children, readMore, logIn, caseStudy, back, ...rest }) {
   const classes = className(rest.className, "font-sans-serif tracking-widest", {
-    "bg-black text-white font-semibold p-[1.25rem] rounded-[0.313rem]": logIn,
+    "bg-black text-white font-semibold py-[1rem] px-[1.75rem] rounded-[0.313rem]": logIn,
     "bg-white text-black py-3.5 px-[1.125rem] rounded-[0.375rem] drop-shadow-[0_5px_4px_rgba(0,0,0,0.25)]":
       readMore,
     "font-light italic border-b-2 border-black": caseStudy || back,
