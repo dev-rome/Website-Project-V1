e6ca1bdb937b76a5c0ecfaee79e75791e86f8b08
dev-rome <haynesjerome@gmail.com>
2023-03-30 01:24:53 -0400

    added gradient colors and edit font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "sans-serif": "Roboto",
+        zwodrei: ["zwodrei Bold", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
       backgroundImage: {
+        "page-background":
+          "linear-gradient(360deg, rgba(170, 210, 233, 0.73) 0%, rgba(174, 207, 226, 0.83) 18.23%, rgba(188, 231, 255, 0.62) 37.8%, rgba(193, 228, 234, 0.948554) 56.45%, #C9ECF1 67.17%, rgba(203, 237, 237, 0.78) 78.74%, rgba(204, 227, 227, 0.61) 89.45%, #E1E8E8 100%);",
+        "hero-background":
+          "linear-gradient(0deg, rgba(63, 106, 96, 0.8), rgba(63, 106, 96, 0.2)), url('/images/forest-front.png'), url('/images/forest-front.png')",
         "hover-gradient":
           "linear-gradient(90deg, #003A5A 0%, #005382 42.71%, #00934C 55.73%, #00522B 100%)",
       },
