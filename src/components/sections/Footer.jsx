c70d1c8e3c5878a07ed7c78676b9b7e387fbc8ed
dev-rome <haynesjerome@gmail.com>
2023-04-06 17:32:08 -0400

    apply responsive changes to footer.jsx for improved user experience

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -9,23 +9,27 @@ import Youtube from "/images/youtube.svg";
 
 function Footer() {
   return (
-    <footer className="relative pt-16 lg:pt-44">
-      <img className="absolute right-0 bottom-0 w-72 lg:w-96" src={Tree} alt="Trees" />
-      <div className="grid grid-cols-5 lg:grid-cols-12 lg:place-items-center lg:gap-x-4 lg:gap-y-3 px-4 lg:px-0">
-        <div className="col-span-2 lg:col-span-2 lg:pl-14">
+    <footer className="relative pt-16 md:pt-40 lg:pt-32">
+      <img
+        className="absolute right-0 bottom-0 w-80 sm:w-96"
+        src={Tree}
+        alt="Trees"
+      />
+      <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 items-center sm:gap-y-3 sm:gap-x-6 md:gap-x-8 lg:gap-y-3 lg:gap-x-10 lg:mt-6 px-4 lg:px-6 pb-2">
+        <div className="col-span-1 md:col-span-2">
           <img src={Logo} alt="Timber & Hatchet" />
-          <p className="font-zwodrei font-bold text-xl text-center">
+          <p className="font-zwodrei font-bold text-base sm:text-lg md:text-lg text-center">
             Built in Boston
           </p>
         </div>
-        <div className="row-start-2 col-span-4 lg:row-span-4 lg:col-span-2">
-          <ul className="mt-4">
-            <li>Contact us</li>
-            <li>Email: Info@Timber.io</li>
-            <li>Phone: (123)456-7890</li>
+        <div className="row-span-4 sm:col-start-2 col-span-4 md:row-start-1 md:col-start-3 lg:row-span-4 lg:col-span-3">
+          <ul className="mt-4 sm:mt-2">
+            <li className="text-sm sm:text-base">Contact us</li>
+            <li className="text-sm sm:text-base">Email: Info@Timber.io</li>
+            <li className="text-sm sm:text-base">Phone: (123)456-7890</li>
           </ul>
           <div className="mt-3">
-            <p className="">Follow us</p>
+            <p>Follow us</p>
             <ul className="flex gap-4 mt-2">
               <li>
                 <a href="#">
@@ -61,14 +65,18 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="relative grid lg:col-start-12 justify-between items-center lg:mt-6 px-4 lg:px-6 pb-2">
-        <p className="font-roboto text-black text-sm lg:text-base mt-4">© 2023 Timber and Hatchet LLC.</p>
-        <ul className="flex lg:flex-row gap-9 lg:gap-16 lg:col-start-9 mt-4">
-          <li className="text-sm lg:text-base">Site Map</li>
-          <li className="text-sm lg:text-base">Accessibility </li>
-          <li className="text-white lg:text-white text-sm lg:text-base">Terms & conditions</li>
-          <li className="text-white lg:text-white text-sm lg:text-base">Privacy Policy</li>
-        </ul>
+      <div className="relative grid lg:col-start-12 items-center lg:mt-6 px-4 lg:px-6 pb-2">
+        <div className="flex flex-col sm:flex-col md:flex-row md:justify-between">
+          <p className="font-roboto text-black text-sm lg:text-base mt-4">
+            © 2023 Timber and Hatchet LLC.
+          </p>
+          <ul className="flex flex-col md:flex-row lg:col-start-9 lg:justify-between gap-5 md:gap-16 mt-4">
+            <li className="text-sm lg:text-base">Site Map</li>
+            <li className="text-sm lg:text-base">Accessibility </li>
+            <li className="md:text-white text-sm lg:text-base">Terms & conditions</li>
+            <li className="md:text-white text-sm lg:text-base">Privacy Policy</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
