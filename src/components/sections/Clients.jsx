47d46b686de2429e96c8cf862717aaef0493dc57
dev-rome <haynesjerome@gmail.com>
2023-04-08 12:49:36 -0400

    add active class to image and set content based on the active class for content display

--- src/components/sections/Clients.jsx
+++ src/components/sections/Clients.jsx
@@ -1,11 +1,54 @@
+import { useState } from "react";
+import { Icon } from "@iconify/react";
 import Staples from "/images/staples-logo.png";
 import ZipCar from "/images/zipcar-logo.png";
-import StaplesRed from "/images/staples-red.png";
 import SectionContainer from "./SectionContainer";
 import Testimonial from "./Testimonial";
 import Button from "../Button";
 
 function Clients() {
+  const [isActive, setIsActive] = useState(false);
+
+  const clientOne = {
+    title: "/images/staples-red.png",
+    augmentations: ["Staffing Augmentation", "3 Month Project", "Team size"],
+    problem:
+      "Staples was facing difficulties in attracting and retaining talented software developers for its development teams. As a result, Staples faced delays in product development, lower productivity, and increased costs.",
+    goal: "The goal of this initiative was to increase the company's pool of skilled software developers.",
+    offer:
+      "With Timber & Hatchet offering nearshore staffing in development teams it offers Staples several benefits to the company, including:",
+    benefits: [
+      "Faster Product Development",
+      "Improved Product Quality",
+      "Reduced Costs",
+      "Increased Productivity",
+    ],
+  };
+
+  const clientTwo = {
+    title: "/images/zipcar-gray.png",
+    augmentations: [
+      "Staffing Augmentation",
+      "5 Month Project",
+      "2-3 Engineers",
+    ],
+    problem:
+      "Zipcar was facing difficulties in attracting and retaining talented software developers for its development teams. As a result, Zipcar faced delays in product development, lower productivity, and increased costs.",
+    goal: "The goal of this initiative was to increase the company's pool of skilled software developers.",
+    offer:
+      "With Timber & Hatchet offering nearshore staffing in development teams it offers Zipcar several benefits to the company, including:",
+    benefits: [
+      "Faster Product Development",
+      "Improved Product Quality",
+      "Reduced Costs",
+      "Increased Productivity",
+    ],
+  };
+
+  const handleToggle = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <SectionContainer className="pt-20 lg:pt-48" id="clients">
       <div className="max-w-5xl mx-auto px-4">
@@ -14,17 +57,39 @@ function Clients() {
         </h2>
         <div className="grid md:grid-cols-5 lg:grid-cols-11 mt-6 lg:mt-12">
           <div className="col-span-2 md:col-span-1 lg:row-start-1 lg:col-span-2 flex gap-3 md:items-start lg:gap-2 md:pr-3">
-            <img src={Staples} alt="Staples" />
-            <img src={ZipCar} alt="Zipcar" />
+            <img
+              className={`cursor-pointer ${
+                isActive ? " " : `border-2 border-black`
+              }`}
+              onClick={handleToggle}
+              src={Staples}
+              alt="Staples"
+            />
+            <img
+              className={`cursor-pointer ${
+                isActive ? `border-2 border-black` : " "
+              }`}
+              onClick={handleToggle}
+              src={ZipCar}
+              alt="Zipcar"
+            />
           </div>
           <div className="col-span-7 md:col-start-3 lg:col-start-5 md:border-l-4 md:border-black md:pl-4">
             <div className="grid grid-cols-5 md:grid-cols-6 lg:grid-cols-3 mt-5 md:mt-0">
-              <img src={StaplesRed} alt="Staples Red" />
+              {!isActive ? (
+                <img src={clientOne.title} alt="Staples" />
+              ) : (
+                <img src={clientTwo.title} alt="Zipcar" />
+              )}
               <div className="col-start-4 col-span-3 md:col-start-4 lg:col-start-5">
                 <ul>
-                  <li className="font-roboto text-lg">Staffing Augmentation</li>
-                  <li className="font-roboto text-lg">3 Month Project</li>
-                  <li className="font-roboto text-lg">Team size</li>
+                  {!isActive
+                    ? clientOne.augmentations.map((augmentation) => (
+                        <li className="font-roboto text-lg">{augmentation}</li>
+                      ))
+                    : clientTwo.augmentations.map((augmentation) => (
+                        <li className="font-roboto text-lg">{augmentation}</li>
+                      ))}
                 </ul>
               </div>
             </div>
@@ -32,32 +97,33 @@ function Clients() {
               <p className="font-roboto text-lg lg:mt-7">Problem</p>
               <div className="mt-4">
                 <p className="font-roboto">
-                  Staples was facing difficulties in attracting and retaining
-                  talented software developers for its development teams. As a
-                  result, Staples faced delays in product development, lower
-                  productivity, and increased costs.
+                  {!isActive ? clientOne.problem : clientTwo.problem}
                 </p>
                 <p className="font-roboto mt-4 lg:mt-6">
-                  The goal of this initiative was to increase the company's pool
-                  of skilled software developers.
+                  {!isActive ? clientOne.goal : clientTwo.goal}
                 </p>
               </div>
               <div className="mt-6">
                 <p className="font-roboto">
-                  With Timber & Hatchet offering nearshore staffing in
-                  development teams it offers Staples several benefits to the
-                  company, including:
+                  {!isActive ? clientOne.offer : clientTwo.offer}
                 </p>
                 <ul className="mt-4 lg:mt-6 pl-4 lg:pl-6">
-                  <li className="list-disc">Faster Product Development</li>
-                  <li className="list-disc">Improved Product Quality</li>
-                  <li className="list-disc">Reduced Costs</li>
-                  <li className="list-disc">Increased Productivity</li>
+                  {!isActive
+                    ? clientOne.benefits.map((benefit) => (
+                        <li className="list-disc font-roboto">{benefit}</li>
+                      ))
+                    : clientTwo.benefits.map((benefit) => (
+                        <li className="list-disc font-roboto">{benefit}</li>
+                      ))}
                 </ul>
               </div>
-              <Button className="flex w-[11.563rem] mt-8 lg:mt-14" caseStudy>
-                Read Full Case Study
-              </Button>
+            </div>
+            <div className="flex items-center mt-8 lg:mt-14">
+              <Button caseStudy>Read Full Case Study</Button>
+              <Icon
+                className="self-end text-2xl"
+                icon="ic:round-keyboard-arrow-right"
+              />
             </div>
           </div>
         </div>
