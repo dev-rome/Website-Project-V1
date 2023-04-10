e4a77bf5baa57256cdefb4fb59b4c491a171e97f
dev-rome <haynesjerome@gmail.com>
2023-04-09 22:13:58 -0400

    added onOpen prop for modal window and testimonial to display data

--- src/components/TestimonialItem.jsx
+++ src/components/TestimonialItem.jsx
@@ -1,18 +1,22 @@
 import Button from "./Button";
 
-function TestimonialItem({ testimonial }) {
+function TestimonialItem({ testimonial, onOpen }) {
   return (
     <div className="lg:max-w-sm bg-white/20 drop-shadow-[4px_4px_4px_rgba(255,255,255,0.5)] rounded-2xl pt-4 px-6 lg:px-2 pb-14">
       <div className="flex items-center gap-3">
-        <img src={testimonial.src} alt="Image One" />
+        <img src={testimonial.src} alt={testimonial.alt} />
         <div>
           <h3 className="font-roboto">{testimonial.title}</h3>
           <p className="font-roboto text-sm">{testimonial.name}</p>
         </div>
       </div>
-      <p className="font-roboto my-6 lg:mt-12 lg:mb-8">{testimonial.description}</p>
+      <p className="font-roboto my-6 lg:mt-12 lg:mb-8">
+        {testimonial.description}
+      </p>
       <div className="flex justify-center">
-        <Button readMore>Read More</Button>
+        <Button onClick={onOpen} readMore>
+          Read More
+        </Button>
       </div>
     </div>
   );
