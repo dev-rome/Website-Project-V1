2824f6fca8e0fc8729cfb0e58650d78eca25dc76
dev-rome <haynesjerome@gmail.com>
2023-04-20 09:52:20 -0400

    change px to rem units and resize and change image positioning

--- src/components/sections/HeroSection/CompositeBackground.jsx
+++ src/components/sections/HeroSection/CompositeBackground.jsx
@@ -1,4 +1,4 @@
-import Image from "../../Image"
+import Image from "../../Image";
 import Tech from "/images/tech-pattern.png";
 import TreeOne from "/images/tree-one.png";
 import TreeTwo from "/images/tree-two.png";
@@ -14,19 +14,19 @@ function CompositeBackground() {
         <img className="w-64 md:w-80 lg:w-96" src={Tech} alt="Tech Pattern" />
       </div>
       <div className="absolute left-0 bottom-0 opacity-40">
-        <img className="w-80" src={TreeOne} alt="Trees" />
+        <img className="w-80 2xl:w-[25.938rem]" src={TreeOne} alt="Trees" />
       </div>
-      <div className="absolute left-[225px] bottom-0 opacity-40">
-        <img className="w-96" src={TreeTwo} alt="Trees" />
+      <div className="absolute hidden sm:block left-[14.063rem] 2xl:left-[17.188rem] bottom-0 opacity-40">
+        <img className="w-96 2xl:w-[40.625rem]" src={TreeTwo} alt="Trees" />
       </div>
-      <div className="absolute left-[600px] bottom-0 opacity-40">
-        <Image src={TreeThree} width={500} alt={"Trees"} />
+      <div className="absolute hidden md:block left-[37.5rem] 2xl:left-[56.25rem] bottom-0 opacity-40">
+        <img className="w-[31.25rem] 2xl:w-[37.5rem]" src={TreeThree} alt="Trees" />
       </div>
-      <div className="absolute left-[950px] bottom-0 opacity-40">
-        <Image src={TreeFour} width={200} alt={"Trees"} />
+      <div className="absolute hidden lg:block left-[59.375rem] 2xl:left-[81.25rem] bottom-0 opacity-40">
+        <img className="w-[12.5rem] 2xl:w-[17.188rem]" src={TreeFour} alt="Trees" />
       </div>
-      <div className="absolute left-[1050px] bottom-0 opacity-40">
-        <Image src={TreeFive} width={400} alt={"Trees"} />
+      <div className="absolute hidden lg:block left-[65.625rem] 2xl:left-[90.625rem] bottom-0 opacity-40">
+        <img className="w-[25rem] 2xl:w-[34.375rem]" src={TreeFive} alt="Trees" />
       </div>
     </>
   );
