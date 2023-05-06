e3e7af2e940027db454d66d53e53807cadc18607
dev-rome <haynesjerome@gmail.com>
2023-05-06 06:05:39 -0400

    change text size and heading size

--- src/components/sections/TestimonialSection/Modal.jsx
+++ src/components/sections/TestimonialSection/Modal.jsx
@@ -9,11 +9,11 @@ function Modal({ testimonial, onClose }) {
     >
       <div className="relative max-w-5xl bg-[#C5E0E3] shadow-[0_10px_4px_rgba(0,0,0,0.25)] backdrop-blur-[2px] rounded-2xl pt-7 px-4 md:px-14 pb-11">
         <div className="grid lg:grid-cols-4">
-          <div className="col-start-1 col-span-3 flex gap-10 mb-6">
+          <div className="col-start-1 col-span-3 flex items-start gap-10 mb-6">
             <img src={testimonial.src} alt={testimonial.alt} />
             <div>
-              <h2 className="font-roboto text-lg">{testimonial.title}</h2>
-              <p className="font-roboto">{testimonial.name}</p>
+              <h2 className="font-roboto md:text-lg">{testimonial.title}</h2>
+              <p className="font-roboto md:text-lg">{testimonial.name}</p>
             </div>
           </div>
           <div className="col-start-4 justify-self-end">
@@ -22,9 +22,9 @@ function Modal({ testimonial, onClose }) {
             </Button>
           </div>
           <div className="grid col-span-4 gap-y-5">
-            <p className="font-roboto">{testimonial.contentOne}</p>
-            <p className="font-roboto">{testimonial.contentTwo}</p>
-            <p className="font-roboto">{testimonial.contentThree}</p>
+            <p className="font-roboto md:text-lg">{testimonial.contentOne}</p>
+            <p className="font-roboto md:text-lg">{testimonial.contentTwo}</p>
+            <p className="font-roboto md:text-lg">{testimonial.contentThree}</p>
           </div>
         </div>
       </div>
