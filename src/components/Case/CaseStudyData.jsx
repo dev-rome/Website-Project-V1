c9410b5fc25cdc0444af0ba80537f93c98ebdb20
dev-rome <haynesjerome@gmail.com>
2023-05-08 10:49:44 -0400

    made changes to case study data and case study info to display case study based on the url slug

---
+++ src/components/Case/CaseStudyData.jsx
@@ -0,0 +1,86 @@
+export const caseStudyData = [
+  {
+    id: 1,
+    src: "/images/staples-red.png",
+    alt: "Staples",
+    slug: "staples",
+    description:
+      "Timber and Hatchet will address Staples challenges in attracting and retaining talented software developers by implementing the following solution:",
+    solution: {
+      solutionList: [
+        [
+          "Research and Identify Potential Staff:",
+          "Timber and Hatchet can research and identify potential nearshore locations that have a large pool of skilled software developers. Timber and Hatchet can help to screen, interview, and provide high-quality developers to work on this project with the company.",
+        ],
+        [
+          "Set Up Remote Teams:",
+          "We curate teams consisting of nearshore software developers to work in collaboration with its existing development teams. These remote teams will provide additional resources and expertise, allowing the company to accelerate product development and increase productivity.",
+        ],
+        [
+          "Communication and Collaboration Tools:",
+          "In order to ensure effective collaboration between its remote and on-site teams, Timber and Hatchet implemented communication and collaboration tools such as project management software, and instant messaging.",
+        ],
+        [
+          "Provide Training and Professional Development:",
+          "Timber and Hatchet provides training and professional development opportunities. This will help to improve the skills of its developers and increase the overall quality of its products.",
+        ],
+      ],
+    },
+    descriptionTwo:
+      "By implementing these steps, Staples can successfully augment its development teams with nearshore staffing, increasing its pool of talented software developers, reducing delays in product development, increasing productivity, and reducing costs.",
+    descriptionThree:
+      "With Timber & Hatchet offering nearshore staffing in development teams it offers Staples several benefits to the company, including:",
+    resultsAndBenefits: {
+      benefitsList: [
+        "Faster Product Development",
+        "Improved Product Quality",
+        "Reduced Costs",
+        "Increased Productivity",
+      ],
+      results:
+        "Overall improve its product development cycle, increase its pool of skilled developers, reduce costs, and enhance customer satisfaction.",
+    },
+  },
+  {
+    id1: 2,
+    src: "/images/zipcar-gray.png",
+    alt: "Zipcar",
+    slug: "zipcar",
+    description:
+      "Timber and Hatchet will address Zipcar challenges in attracting and retaining talented software developers by implementing the following solution:",
+    solution: {
+      solutionList: [
+        [
+          "Research and Identify Potential Staff:",
+          "Timber and Hatchet can research and identify potential nearshore locations that have a large pool of skilled software developers. Timber and Hatchet can help to screen, interview, and provide high-quality developers to work on this project with the company.",
+        ],
+        [
+          "Set Up Remote Teams:",
+          "We curate teams consisting of nearshore software developers to work in collaboration with its existing development teams. These remote teams will provide additional resources and expertise, allowing the company to accelerate product development and increase productivity.",
+        ],
+        [
+          "Communication and Collaboration Tools:",
+          "In order to ensure effective collaboration between its remote and on-site teams, Timber and Hatchet implemented communication and collaboration tools such as project management software, and instant messaging.",
+        ],
+        [
+          "Provide Training and Professional Development:",
+          "Timber and Hatchet provides training and professional development opportunities. This will help to improve the skills of its developers and increase the overall quality of its products.",
+        ],
+      ],
+    },
+    descriptionTwo:
+      "By implementing these steps, Staples can successfully augment its development teams with nearshore staffing, increasing its pool of talented software developers, reducing delays in product development, increasing productivity, and reducing costs.",
+    descriptionThree:
+      "With Timber & Hatchet offering nearshore staffing in development teams it offers Zipcar several benefits to the company, including:",
+    resultsAndBenefits: {
+      benefitsList: [
+        "Enhanced User Experience",
+        "Streamlined Booking Process",
+        "Improved Platform Stability",
+        "Scalable Infrastructure",
+      ],
+      results:
+        "Overall improve its product development cycle, increase its pool of skilled developers, reduce costs, and enhance customer satisfaction.",
+    },
+  },
+];
--- src/components/Case/CaseStudyInfo.jsx
+++ src/components/Case/CaseStudyInfo.jsx
@@ -1,9 +1,20 @@
 import { Icon } from "@iconify/react";
-import Staples from "/images/staples-red.png";
+import { caseStudyData } from "./CaseStudyData";
 import SectionContainer from "../SectionContainer";
 import Button from "../Button";
 
-function CaseStudyInfo() {
+function CaseStudyInfo({ slug }) {
+  const caseStudy = caseStudyData.find((data) => data.slug === slug);
+  const {
+    src,
+    alt,
+    description,
+    descriptionTwo,
+    descriptionThree,
+    solution,
+    resultsAndBenefits,
+  } = caseStudy;
+
   const handleBack = () => {
     window.location.href = "/";
   };
@@ -13,7 +24,7 @@ function CaseStudyInfo() {
       <div className="max-w-5xl mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-12">
           <div className="col-span-12 flex flex-col sm:flex-row justify-between gap-8">
-            <img className="w-44 order-2 sm:order-none" src={Staples} alt="" />
+            <img className="w-44 order-2 sm:order-none" src={src} alt={alt} />
             <div className="flex items-center">
               <Button caseStudy onClick={handleBack}>
                 All Our Case Studies
@@ -22,84 +33,29 @@ function CaseStudyInfo() {
             </div>
           </div>
           <div className="col-span-12 mt-10">
-            <p className="font-roboto">
-              Timber and Hatchet will address Staples challenges in attracting
-              and retaining talented software developers by implementing the
-              following solution:
-            </p>
+            <p className="font-roboto">{description}</p>
             <ol className="list-decimal font-roboto pl-6">
-              <li>
-                <p>
-                  <span className="font-extrabold">
-                    Research and Identify Potential Staff:
-                  </span>
-                  Timber and Hatchet can research and identify potential
-                  nearshore locations that have a large pool of skilled software
-                  developers. Timber and Hatchet can help to screen, interview,
-                  and provide high-quality developers to work on this project
-                  with the company.
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className="font-extrabold">Set Up Remote Teams:</span>{" "}
-                  We curate teams consisting of nearshore software developers to
-                  work in collaboration with its existing development teams.
-                  These remote teams will provide additional resources and
-                  expertise, allowing the company to accelerate product
-                  development and increase productivity.
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className="font-extrabold">
-                    Communication and Collaboration Tools:
-                  </span>
-                  In order to ensure effective collaboration between its remote
-                  and on-site teams, Timber and Hatchet implemented
-                  communication and collaboration tools such as project
-                  management software, and instant messaging.
-                </p>
-              </li>
-              <li>
-                <p>
-                  <span className="font-extrabold">
-                    Provide Training and Professional Development:
-                  </span>
-                  Timber and Hatchet provides training and professional
-                  development opportunities. This will help to improve the
-                  skills of its developers and increase the overall quality of
-                  its products.
-                </p>
-              </li>
+              {solution.solutionList.map(([title, item], index) => {
+                return (
+                  <li key={index + 1}>
+                    <span className="font-extrabold">{title}</span> {item}
+                  </li>
+                );
+              })}
             </ol>
-            <p className="font-roboto">
-              By implementing these steps, Staples can successfully augment its
-              development teams with nearshore staffing, increasing its pool of
-              talented software developers, reducing delays in product
-              development, increasing productivity, and reducing costs.
-            </p>
+            <p className="font-roboto">{descriptionTwo}</p>
           </div>
           <p className="col-span-3 font-roboto font-bold text-lg mt-10">
             Results and Benefits
           </p>
           <div className="col-span-12 mt-5">
-            <p className="font-roboto">
-              With Timber & Hatchet offering nearshore staffing in development
-              teams it offers Staples several benefits to the company,
-              including:
-            </p>
+            <p className="font-roboto">{descriptionThree}</p>
             <ul className="list-disc font-roboto pl-6">
-              <li>Faster Product Development</li>
-              <li>Improved Product Quality</li>
-              <li>Reduced Costs</li>
-              <li>Increased Productivity</li>
+              {resultsAndBenefits.benefitsList.map((item, index) => {
+                return <li key={index + 1}>{item}</li>;
+              })}
             </ul>
-            <p className="font-roboto pb-10">
-              Overall improve its product development cycle, increase its pool
-              of skilled developers, reduce costs, and enhance customer
-              satisfaction.
-            </p>
+            <p className="font-roboto pb-10">{resultsAndBenefits.results}</p>
           </div>
         </div>
       </div>
