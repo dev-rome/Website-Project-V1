4b25a8d31fe93365fb048cd10f8c23e599553fd4
dev-rome <haynesjerome@gmail.com>
2023-05-11 11:51:38 -0400

    edit files to fix render issues

--- src/components/sections/HeroSection/LogoCarousel.jsx
+++ src/components/sections/HeroSection/LogoCarousel.jsx
@@ -1,58 +1,88 @@
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/alice-carousel.css";
+// import AliceCarousel from "react-alice-carousel";
+// import "react-alice-carousel/lib/alice-carousel.css";
+
+import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 function LogoCarousel() {
+  // const items = [
+  //   <img
+  //     className="w-[85px]"
+  //     src="/images/staples.png"
+  //     role="presentation"
+  //     alt="Staples"
+  //   />,
+  //   <img
+  //     className="w-[85px]"
+  //     src="/images/zipcar.png"
+  //     role="presentation"
+  //     alt="Zipcar"
+  //   />,
+  //   <img
+  //     className="w-[85px]"
+  //     src="/images/ellipse.png"
+  //     role="presentation"
+  //     alt="Ellipse"
+  //   />,
+  //   <img
+  //     className="w-[85px]"
+  //     src="/images/indigo.png"
+  //     role="presentation"
+  //     alt="Indigo"
+  //   />,
+  //   <img
+  //     className="w-[85px]"
+  //     src="/images/gloo.png"
+  //     role="presentation"
+  //     alt="Gloo"
+  //   />,
+  // ];
+
+  // const responsive = {
+  //   0: { items: 2 },
+  //   640: { items: 3 },
+  //   1024: { items: 4 },
+  // };
+
+  // return (
+  //   <AliceCarousel
+  //     items={items}
+  //     responsive={responsive}
+  //     infinite={true}
+  //     autoPlay={true}
+  //     autoPlayInterval={2000}
+  //     animationDuration={600}
+  //     animationEasingFunction="linear"
+  //     disableDotsControls
+  //     disableButtonsControls
+  //   />
+  // );
   const items = [
-    <img
-      className="w-[85px]"
-      src="/images/staples.png"
-      role="presentation"
-      alt="Staples"
-    />,
-    <img
-      className="w-[85px]"
-      src="/images/zipcar.png"
-      role="presentation"
-      alt="Zipcar"
-    />,
-    <img
-      className="w-[85px]"
-      src="/images/ellipse.png"
-      role="presentation"
-      alt="Ellipse"
-    />,
-    <img
-      className="w-[85px]"
-      src="/images/indigo.png"
-      role="presentation"
-      alt="Indigo"
-    />,
-    <img
-      className="w-[85px]"
-      src="/images/gloo.png"
-      role="presentation"
-      alt="Gloo"
-    />,
+    "/images/staples.png",
+    "/images/zipcar.png",
+    "/images/ellipse.png",
+    "/images/indigo.png",
+    "/images/gloo.png",
   ];
 
-  const responsive = {
-    0: { items: 2 },
-    640: { items: 3 },
-    1024: { items: 4 },
-  };
-
   return (
-    <AliceCarousel
-      items={items}
-      responsive={responsive}
+    <CarouselProvider
+      naturalSlideWidth={100}
+      naturalSlideHeight={50}
+      totalSlides={items.length}
       infinite={true}
-      autoPlay={true}
-      autoPlayInterval={2000}
-      animationDuration={600}
-      animationEasingFunction="linear"
-      disableDotsControls
-      disableButtonsControls
-    />
+      isIntrinsicHeight={true}
+      isPlaying={true}
+      interval={2000}
+    >
+      <Slider>
+        {items.map((item, index) => (
+          <Slide index={index} key={index}>
+            <img className="w-[85px]" src={item} role="presentation" alt="" />
+          </Slide>
+        ))}
+      </Slider>
+    </CarouselProvider>
   );
 }
 
