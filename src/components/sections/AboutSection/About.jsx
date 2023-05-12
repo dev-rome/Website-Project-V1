45dabfee72e8c951ff2be16c50d964a0b33b32f6
dev-rome <haynesjerome@gmail.com>
2023-05-12 16:00:22 -0400

    added react-intersection-observer to animate the text image for when user scrolls into view

--- src/components/sections/AboutSection/About.jsx
+++ src/components/sections/AboutSection/About.jsx
@@ -1,21 +1,51 @@
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import SectionContainer from "../../SectionContainer";
-import Logo from "/images/timber-hatchet-logo.svg";
+import Logo from "/images/hatchet-logo.png";
+import Timber from "/images/timber-text.png";
+import Hatchet from "/images/hatchet-text.png";
 import Support from "/images/support.png";
 import Collaboration from "/images/collaboration.png";
 import Transparency from "/images/transparency.png";
 
 function About() {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 1,
+  });
+
   return (
     <SectionContainer className="pt-20 lg:pt-48" id="about">
       <div className="max-w-5xl mx-auto px-4">
         <div className="grid md:grid-cols-8 lg:grid-cols-11">
           <div className="md:col-span-4 lg:col-span-5">
             <div className="grid grid-cols-3 lg:grid-cols-5">
-              <img
-                className="w-[23.438rem] sm:w-[28.125rem] md:w-auto mx-auto col-span-3 md:col-span-4 lg:col-span-5"
-                src={Logo}
-                alt="Timber & Hatchet"
-              />
+              <div
+                ref={ref}
+                className="flex justify-center items-center col-start-2 lg:col-start-3 w-36"
+              >
+                {inView && (
+                  <motion.img
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="relative right-[-0.5rem] top-[0.5rem] w-44"
+                    src={Timber}
+                    alt="Image of the text Timber"
+                  />
+                )}
+                <img className="w-[5rem]" src={Logo} alt="Timber & Hatchet" />
+                {inView && (
+                  <motion.img
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="relative right-[2.2rem] top-[0.6rem] w-44"
+                    src={Hatchet}
+                    alt="Image of the text Hatchet"
+                  />
+                )}
+              </div>
               <p className="col-span-4 row-start-2 lg:col-span-5 font-roboto mt-4">
                 We have a software development and cross-industry experience
                 that gives us a unique perspective when looking for the right
@@ -31,7 +61,7 @@ function About() {
           </div>
           <div className="md:col-start-6 lg:col-start-8">
             <div className="grid grid-cols-6 md:grid-cols-1 lg:grid-cols-1 gap-y-14 lg:gap-y-20 mt-11 md:mt-0">
-              <div className="flex items-center gap-2 md:pt-20 lg:pt-24">
+              <div className="flex items-center gap-2 md:pt-20">
                 <img src={Support} alt="Support" />
                 <p className="font-zwodrei font-bold text-lg sm:text-2xl">
                   Support
