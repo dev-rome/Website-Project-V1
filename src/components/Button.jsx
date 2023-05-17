e7368b7de9204e7b01d5087991769a31e08c44cd
dev-rome <haynesjerome@gmail.com>
2023-05-17 19:13:23 -0400

    edit button names

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import className from "classnames";
 
-function Button({ children, readMore, logIn, caseStudy, back, ...rest }) {
+function Button({ children, ReadMoreButton, LogInButton, CaseStudyButton, AllCaseStudyButton, ...rest }) {
   const classes = className(rest.className, "font-sans-serif tracking-widest", {
-    "bg-black text-white font-semibold py-[1rem] px-[1.75rem] rounded-[0.313rem]": logIn,
+    "bg-black text-white font-semibold py-[1rem] px-[1.75rem] rounded-[0.313rem]": LogInButton,
     "bg-white text-black py-3.5 px-[1.125rem] rounded-[0.375rem] drop-shadow-[0_5px_4px_rgba(0,0,0,0.25)]":
-      readMore,
-    "font-light italic border-b-2 border-black": caseStudy || back,
+      ReadMoreButton,
+    "font-light italic border-b-2 border-black": CaseStudyButton || AllCaseStudyButton,
   });
 
   return (
