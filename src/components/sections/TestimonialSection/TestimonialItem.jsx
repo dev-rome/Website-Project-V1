e6bae9dbf992940e358289d8d4b9809d701c58a1
dev-rome <haynesjerome@gmail.com>
2023-05-17 20:11:47 -0400

    destructuring testimonial

--- src/components/sections/TestimonialSection/TestimonialItem.jsx
+++ src/components/sections/TestimonialSection/TestimonialItem.jsx
@@ -1,18 +1,18 @@
 import Button from "../../Button";
 
 function TestimonialItem({ testimonial, onOpen }) {
+  const { src, alt, title, name, description } = testimonial;
+
   return (
     <div className="lg:max-w-sm bg-white/20 drop-shadow-[4px_4px_4px_rgba(255,255,255,0.5)] rounded-2xl pt-4 px-6 lg:px-2 pb-14">
       <div className="flex items-center gap-3">
-        <img src={testimonial.src} alt={testimonial.alt} />
+        <img src={src} alt={alt} />
         <div>
-          <h3 className="font-roboto">{testimonial.title}</h3>
-          <p className="font-roboto text-sm">{testimonial.name}</p>
+          <h3 className="font-roboto">{title}</h3>
+          <p className="font-roboto text-sm">{name}</p>
         </div>
       </div>
-      <p className="font-roboto my-6 lg:mt-12 lg:mb-8">
-        {testimonial.description}
-      </p>
+      <p className="font-roboto my-6 lg:mt-12 lg:mb-8">{description}</p>
       <div className="flex justify-center">
         <Button onClick={onOpen} ReadMoreButton>
           Read More
