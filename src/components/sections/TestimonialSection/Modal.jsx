004d3b501c4e6f7be22ff3cf9fbdcb862608bae8
dev-rome <haynesjerome@gmail.com>
2023-06-14 12:47:16 -0400

    removed button

--- src/components/sections/TestimonialSection/Modal.jsx
+++ src/components/sections/TestimonialSection/Modal.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { Icon } from "@iconify/react";
-import Button from "../../Button";
 
 function Modal({ testimonial, onClose }) {
   const { src, alt, title, name, contentOne, contentTwo, contentThree } =
@@ -14,11 +13,10 @@ function Modal({ testimonial, onClose }) {
   }, []);
 
   return (
-      <div
-        onClick={onClose}
-        className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-10 px-4"
-      >
-
+    <div
+      onClick={onClose}
+      className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-10 px-4"
+    >
       <div className="relative max-w-5xl 2xl:max-w-6xl bg-[#C5E0E3] shadow-[0_10px_4px_rgba(0,0,0,0.25)] backdrop-blur-[2px] rounded-2xl pt-7 px-4 md:px-14 pb-11">
         <div className="grid lg:grid-cols-4">
           <div className="col-start-1 col-span-3 flex items-start gap-10 mb-6">
@@ -29,9 +27,11 @@ function Modal({ testimonial, onClose }) {
             </div>
           </div>
           <div className="col-start-4 justify-self-end">
-            <Button onClick={onClose}>
-              <Icon className="text-3xl" icon="ic:baseline-close" />
-            </Button>
+            <Icon
+              className="text-3xl cursor-pointer"
+              icon="ic:baseline-close"
+              onClick={onClose}
+            />
           </div>
           <div className="grid col-span-4 gap-y-5">
             <p className="font-roboto md:text-lg 2xl:text-xl">{contentOne}</p>
@@ -40,7 +40,7 @@ function Modal({ testimonial, onClose }) {
           </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
