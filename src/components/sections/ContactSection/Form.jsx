07aec4ef3b1f30ae9228a50d26170e5cfafecde4
dev-rome <haynesjerome@gmail.com>
2023-06-22 18:39:17 -0400

    added type submit to button

--- src/components/sections/ContactSection/Form.jsx
+++ src/components/sections/ContactSection/Form.jsx
@@ -55,7 +55,7 @@ function Form({ onSubmit }) {
         value={formData.message}
         onChange={handleChange}
       ></textarea>
-      <Button className="custom-form-button custom-form-submit-button">
+      <Button type="submit" className="custom-form-button custom-form-submit-button">
         Submit
       </Button>
     </form>
