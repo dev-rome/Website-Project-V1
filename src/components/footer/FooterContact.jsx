902af1f792c503a8e5534116a76345735374ca57
dev-rome <haynesjerome@gmail.com>
2023-06-22 20:05:47 -0400

    change footer contact margin

--- src/components/footer/FooterContact.jsx
+++ src/components/footer/FooterContact.jsx
@@ -1,6 +1,6 @@
 function FooterContact() {
   return (
-    <ul className="mt-4 sm:mt-2">
+    <ul className="mt-4 sm:mt-2 md:mt-6">
       <li className="font-roboto text-sm sm:text-base">Contact us</li>
       <li className="font-roboto text-sm sm:text-base">
         Email: Info@Timber.io
